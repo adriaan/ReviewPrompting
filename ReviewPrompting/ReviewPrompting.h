496719c279591d7a57a442ebb3e40815f2240508
Adriaan Stellingwerff <adriaan@fieldapps.co>
2018-07-19 11:15:33 +1000

    Initial commit

---
+++ ReviewPrompting/ReviewPrompting.h
@@ -0,0 +1,19 @@
+//
+//  ReviewPrompting.h
+//  ReviewPrompting
+//
+//  Created by Adriaan on 19/7/18.
+//  Copyright © 2018 Field Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReviewPrompting.
+FOUNDATION_EXPORT double ReviewPromptingVersionNumber;
+
+//! Project version string for ReviewPrompting.
+FOUNDATION_EXPORT const unsigned char ReviewPromptingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReviewPrompting/PublicHeader.h>
+
+
